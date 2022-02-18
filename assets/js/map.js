6e3806ccca32231c757892b2d8e1c47b6a6393ff
PedroCalasBlasco <pedrocalasblasco@gmail.com>
2022-02-18 11:31:41 -0300

    campanas visual

--- assets/js/map.js
+++ assets/js/map.js
@@ -10,12 +10,20 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
 var map = L.map('map', {
     center: [-31.6166, -60.7117],
     zoom: 13, 
-    layers: [osm, Esri_WorldImagery]
+    layers: [Esri_WorldImagery, osm]
 
 });
 
-var marcadorEscuelas = L.AwesomeMarkers.icon({
+var marcadorEcoPuntos = L.AwesomeMarkers.icon({
     icon: 'recycle',
+    prefix:'fa',
+    markerColor: 'darkgreen',
+    iconColor: 'white'
+  });
+
+
+  var marcadorEcoPuntosCampanas = L.AwesomeMarkers.icon({
+    icon: 'bell',
     prefix:'fa',
     markerColor: 'green',
     iconColor: 'white'
@@ -35,8 +43,9 @@ var obj;
 let layerEcoPuntos;
 
 var baseMaps = {
+    "Imagen": Esri_WorldImagery,
     "Street Map" : osm,
-    "Imagen": Esri_WorldImagery
+    
 }
 
 
@@ -50,8 +59,8 @@ fetch('https://raw.githubusercontent.com/PedroCalasBlasco/munirepo/main/assets/j
 
     let layerEcoPuntos = L.geoJson(data,{
         onEachFeature: function(feature, layer){
-            layer.bindPopup("<h3>" + feature.properties.Name + "</h3>");
-            layer.setIcon(marcadorEscuelas);
+            layer.bindPopup("<h3>EcoPuntos con Atención Personalizada</h3><h2>" + feature.properties.Name + "</h2><p><strong>¿Qué podes llevar?</strong></br> Papel, cartón, plástico, latas, vidrio y aparatos eléctricos y electrónicos en desuso.</p><p><strong>Horario de Atención</strong></br> 9 a 19 hs.</p>");
+            layer.setIcon(marcadorEcoPuntos);
         }
     });
     map.addLayer(layerEcoPuntos);
@@ -66,8 +75,36 @@ fetch('https://raw.githubusercontent.com/PedroCalasBlasco/munirepo/main/assets/j
 
   });
 
-    
 
-// var recycle = L.marker([-31.6333294, -60.6900008], {icon:marcadorEscuelas});
-// recycle.bindPopup("<h3>Puntos de Reciclaje</h3><p>Estación Belgrano</p>").openPopup();
+
+  fetch(' https://raw.githubusercontent.com/PedroCalasBlasco/munirepo/main/assets/json/campanas.geojson')
+  .then(res => res.json())
+  .then(data => { 
+    
+    console.log(data);
+
+    let layerEcoPuntosCampanas = L.geoJson(data,{
+        onEachFeature: function(feature, layer){
+            layer.bindPopup("<h3>EcoPuntos Sin Atención Personalizada</h3><h2>" + feature.properties.nombre + "</h2><p><strong>¿Qué podes llevar?</strong></br> Papel, cartón, plástico, latas, vidrio</p>");
+            layer.setIcon(marcadorEcoPuntosCampanas);
+        }
+    });
+    map.addLayer(layerEcoPuntosCampanas);
+    console.log(layerEcoPuntosCampanas);
+    
+    // var overlay = L.layerGroup([layerEcoPuntos]).addTo(map);
+
+    // var overlayMaps =  {
+    //     "<span style='color: green'>Reciclaje</span>": overlay,
+    // }
+    
+    // L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+  });
+
+
+
+ 
+
+
 
